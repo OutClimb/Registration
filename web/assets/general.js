275321c285dc767a502f93ec371420f7ac910372
Nickel Mattera <me@nicholemattera.gay>
2025-10-13 16:38:59 -0500

    feat: add general template

---
+++ web/assets/general.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('generalForm');
+    const errors = document.querySelectorAll('.error');
+
+    // Prevent multiple form submissions
+    let submissionInProgress = false;
+
+    // Handle form submission
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault();
+        
+        let isValid = true;
+        let firstError = null;
+
+        // Exit early if a submission is already in progress
+        if (submissionInProgress) {
+            return;
+        }
+
+        // Reset errors
+        errors.forEach(error => error.classList.add('hidden'));
+
+        // Validate required fields
+        ['name', 'email'].forEach(field => {
+            const input = document.getElementById(field);
+            if (!input.value.trim()) {
+                document.getElementById(field + 'Error').classList.remove('hidden');
+                if (!firstError) firstError = field;
+                isValid = false;
+                return;
+            }
+
+            if (input.dataset.validation && !new RegExp(input.dataset.validation).test(input.value)) {
+                document.getElementById(field + 'FormatError').classList.remove('hidden');
+                if (!firstError) firstError = field;
+                isValid = false;
+            }
+        });
+
+        if (isValid && !submissionInProgress) {
+            grecaptcha.ready(function() {
+                grecaptcha.execute(document.getElementById('recaptchaSiteKey').value, {action: 'submit'}).then(async function(token) {
+                    submissionInProgress = true;
+                    document.getElementById('submitButton').disabled = true;
+
+                    const formSlug = document.getElementById('formSlug').value;
+                    const response = await fetch(`/api/v1/submission/${formSlug}`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            name: document.getElementById('name').value,
+                            pronouns: document.getElementById('pronouns').value,
+                            email: document.getElementById('email').value,
+                        })
+                    });
+
+                    if (response.status === 201) {
+                        document.getElementById('successMessage').classList.remove('hidden');
+                        form.classList.add('hidden');
+                    } else {
+                        const errorData = await response.json();
+                        if (errorData.error) {
+                            document.getElementById('errorMessage').innerText = 'An error occurred while submitting the form. Please try again. (' + response.status + ' - ' + errorData.error + ')';
+                        } else {
+                            document.getElementById('errorMessage').innerText = 'An error occurred while submitting the form. Please try again. (' + response.status + ')';
+                        }
+                        document.getElementById('errorMessage').classList.remove('hidden');
+                    }
+                
+                    submissionInProgress = false;
+                });
+            });
+        } else if (firstError) {
+            // Focus on the first error field and scroll into the view.
+            document.getElementById(firstError).focus();
+            document.getElementById(firstError).scrollIntoView();
+        }
+    });
+});
